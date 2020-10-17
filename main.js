2c15cb404010093accb80b5abe8c9c5144bd1fc6
Kirk Hahn <kirkhahn11@yahoo.com>
2020-10-17 14:21:16 -0500

    redo of user can select day to view events

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var daysOfTheWeek = document.querySelectorAll('.days-of-week');
-var scheduleText = document.getElementById('schedule-text')
+var scheduleText = document.getElementById('schedule-text');
+var container = document.getElementById('container');
 
-for (var i = 0; i < daysOfTheWeek.length; i++) {
-  daysOfTheWeek[i].addEventListener('click', addEvent)
-}
+container.addEventListener('click', addEvent);
 
 function addEvent() {
-  scheduleText.textContent = 'Scheduled Events for ' + event.target.textContent
-  console.log(event.target)
+  if (event.target.textContent === 'Sunday' || event.target.textContent === 'Monday' ||
+    event.target.textContent === 'Tuesday' || event.target.textContent === 'Wednesday' ||
+    event.target.textContent === 'Thursday' || event.target.textContent === 'Friday' ||
+    event.target.textContent === 'Saturday') {
+      scheduleText.textContent = 'Scheduled Events For ' + event.target.textContent
+      console.log(event.target)
+  }
 }
